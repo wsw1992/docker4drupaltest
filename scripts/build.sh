3cbe0fa928ee1aee423b69cd3ebd764cb1bc8488
wsw1992 <46236671+wsw1992@users.noreply.github.com>
2019-01-23 16:39:08 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 docker run --rm \
     -v "$HOME/.composer":/composer \
     -v "$PWD":/app \
-    composer/composer:alpine create-project drupal-composer/drupal-project:8.x-dev my_site_name_dir --stability dev --no-interaction -n
+    composer/composer:alpine -n
 
 # Install all requirements.
 docker run --rm \
