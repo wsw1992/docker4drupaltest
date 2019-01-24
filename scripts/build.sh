0b25914a4ef3347f6157e2ed18af764cab7c4fc0
wsw1992 <46236671+wsw1992@users.noreply.github.com>
2019-01-24 11:02:46 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,11 @@ docker run --rm \
     -v "$HOME/.composer":/composer \
     -v "$PWD":/app \
     composer/composer:alpine 
-docker ps
+docker ps -a
 
 # Install all requirements.
 #docker run --rm \
 #    -v "$HOME/.composer":/composer \
 #    -v "$PWD":/app \
 #    composer/composer:alpine update -n
-docker exec alpine composer create-project drupal-composer/drupal-project:8.x-dev my_site_name_dir --stability dev --no-interaction
+docker exec composer composer create-project drupal-composer/drupal-project:8.x-dev my_site_name_dir --stability dev --no-interaction
