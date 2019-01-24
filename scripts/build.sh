138fe72e69c38a16f44a713a8c7b9c7fcd334b97
wsw1992 <46236671+wsw1992@users.noreply.github.com>
2019-01-24 11:08:02 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,4 +14,4 @@ docker ps -a
 #    -v "$HOME/.composer":/composer \
 #    -v "$PWD":/app \
 #    composer/composer:alpine update -n
-docker exec composer composer create-project drupal-composer/drupal-project:8.x-dev my_site_name_dir --stability dev --no-interaction
+docker exec optimistic_shirley composer create-project drupal-composer/drupal-project:8.x-dev my_site_name_dir --stability dev --no-interaction
