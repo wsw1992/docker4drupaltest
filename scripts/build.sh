ffa32466960519bab0ae2462fcba239da6928436
wsw1992 <46236671+wsw1992@users.noreply.github.com>
2019-02-26 14:51:32 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-set -eo pipefail
-
-# Install merge plugin.
-docker run --rm \
-    -v "$HOME/.composer":/composer \
-    -v "$PWD":/app \
-    composer/composer:alpine update -n
+echo "$HOME/"
